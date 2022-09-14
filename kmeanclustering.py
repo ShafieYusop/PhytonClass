1067b76d3e208248a791af5124cf41996244f319
ShafieYusop <113412859+ShafieYusop@users.noreply.github.com>
2022-09-14 11:44:38 +0800

    Update kmeanclustering.py

--- kmeanclustering.py
+++ kmeanclustering.py
@@ -6,14 +6,7 @@ df = pd.read_csv(file)
 features = ['Annual_Income_(k$)', 'Spending_Score']
 X = df[features]
 
-kmeans = KMeans(n_clusters=5)
-kmeans.fit(X)
-y_kmeans = kmeans.predict(X)
-plt.scatter(X['Annual_Income_(k$)'], X['Spending_Score'], c=y_kmeans, s=50, cmap='viridis')
-
-centers = kmeans.cluster_centers_
-plt.scatter(centers[:,0], centers[:,1], c='black', s=200, alpha=0.5)
-
+plt.scatter(X['Annual_Income_(k$)'], X['Spending_Score']);
 step_size = 0.01
 
 x_min, x_max = min(X.iloc[:,0]) - 1, max(X.iloc[:,0]) + 1
